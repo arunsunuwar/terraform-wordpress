efe28c4058031b6e5f7cd6a9d1cdb2de76bd1ec3
arunsunuwar <48924290+arunsunuwar@users.noreply.github.com>
2020-03-06 16:26:20 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "terraform-wordpress"
     key    = "terraform/dev/ecs-wordpress"
     region = "us-east-1"
+    shared_credentials_file = "/home/intelycorellc/.aws/credentials"
     profile = "arun"
   }
 }
